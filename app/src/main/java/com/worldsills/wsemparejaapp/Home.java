2013bed3e8dfe8b2cf0232717c5ceabbd9bc71f4
Sofia Arciniegas <sofiarciniegas17@gmail.com>
2018-08-21 11:09:57 -0500

    Origin

--- app/src/main/java/com/worldsills/wsemparejaapp/Home.java
+++ app/src/main/java/com/worldsills/wsemparejaapp/Home.java
@@ -1,21 +1,61 @@
 package com.worldsills.wsemparejaapp;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class Home extends AppCompatActivity {
 
+    private String player1, player2;
+    private int player=0;
+    Dialog scores, names, settings, dificultad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ///DIALOGS//////
+
+        scores= new Dialog(this);
+        scores.setContentView(R.layout.dialog_scores);
+        scores.setCanceledOnTouchOutside(false);
+        scores.setCancelable(false);
+        scores.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+
+        names= new Dialog(this);
+        names.setContentView(R.layout.dialog_names);
+        names.setCanceledOnTouchOutside(false);
+        names.setCancelable(false);
+        names.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+
+        settings= new Dialog(this);
+        settings.setContentView(R.layout.dialog_time);
+        settings.setCanceledOnTouchOutside(false);
+        settings.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        scores= new Dialog(this);
+        scores.setContentView(R.layout.dialog_scores);
+        scores.setCanceledOnTouchOutside(false);
+        scores.setCancelable(false);
+        scores.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
     }
 
 
+    private void ingresarNombres(){
 
+
+
+
+    }
 
     public void jugar(View v){
 

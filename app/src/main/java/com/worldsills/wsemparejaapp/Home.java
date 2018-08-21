ddbdfef0487d38641bddee78e5739be1da2b05c9
Sofia Arciniegas <sofiarciniegas17@gmail.com>
2018-08-21 10:49:59 -0500

    Origin

--- app/src/main/java/com/worldsills/wsemparejaapp/Home.java
+++ app/src/main/java/com/worldsills/wsemparejaapp/Home.java
@@ -1,7 +1,9 @@
 package com.worldsills.wsemparejaapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+
+
+
+    public void jugar(View v){
+
+        Intent intent= new Intent(this, Game.class);
+        startActivity(intent);
     }
 }
